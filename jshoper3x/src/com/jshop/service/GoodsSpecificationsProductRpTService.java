af3be1d25463d4417434a24e6b7c2e5ecb18b842
chen da <sdywcd@gmail.com>
2013-08-27 21:36:18 +0800

    add db file

---
+++ jshoper3x/src/com/jshop/service/GoodsSpecificationsProductRpTService.java
@@ -0,0 +1,53 @@
+package com.jshop.service;
+
+import java.util.List;
+
+import com.jshop.entity.GoodsSpecificationsProductRpT;
+
+
+public interface GoodsSpecificationsProductRpTService {
+	
+	/**
+	 * 检测商品和商品规格关系
+	 * @return
+	 */
+	public List<GoodsSpecificationsProductRpT>checkSpecificationRelationshipByspecificationsid(String specificationsid );
+	
+	
+	/**
+	 * 通过goodssetid 查找规格
+	 * @return
+	 */
+	public List<GoodsSpecificationsProductRpT>checkSpecificationRelationshipBygoodssetid(String goodsid);
+	/**
+	 * 通过productid 查找规格
+	 * @return
+	 */
+	public List<GoodsSpecificationsProductRpT>checkSpecificationRelationshipByproductid(String productid);
+
+	/**
+	 * 增加商品规格
+	 * @param goodsSetId
+	 * @param specidicationsId
+	 * @return
+	 */
+	public void addGoodsAssociatedProductById(GoodsSpecificationsProductRpT gsrt);
+	
+	/**
+	 * 更新商品规格
+	 * @param goodsSetId
+	 * @param specidicationsId
+	 * @return
+	 */
+	public void updateGoodsAssociatedProductById(GoodsSpecificationsProductRpT gsrt);
+	
+	/**
+	 * 删除商品规格
+	 * @param goodsSetId
+	 * @return
+	 */
+	public int delGoodsAssociatedProductById(final String goodsid);
+	
+	
+	
+}
